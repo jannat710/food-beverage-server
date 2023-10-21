fbffeb9bd5d7e4b1ecfb22b420fa85707e268bab
e <jannatul0040@gmail.com>
2023-10-21 19:16:51 +0600

    created data to the database using post

--- index.js
+++ index.js
@@ -30,6 +30,16 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
+
+    const foodCollection=client.db("foodDB").collection("foods");
+    app.post('/foods', async (req, res) => {
+        const food = req.body;
+        console.log(food);
+        const result = await foodCollection.insertOne(food);
+        res.send(result);
+    });
+
+
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
